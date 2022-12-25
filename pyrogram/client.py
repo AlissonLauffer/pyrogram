e4249abc64108e17d8abd517ce5530ea94b626ed
Hitalo <hitalo331@outlook.com>
2022-12-25 16:23:19 -0300

    Improved pyromod code

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -44,7 +44,7 @@ from pyrogram.errors import CDNFileHashMismatch
 from pyrogram.errors import (
     SessionPasswordNeeded,
     VolumeLocNotFound, ChannelPrivate,
-    BadRequest
+    BadRequest, ListenerCanceled
 )
 from pyrogram.handlers.handler import Handler
 from pyrogram.methods import Methods
